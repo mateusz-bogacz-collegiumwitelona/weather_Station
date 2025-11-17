91a21fb4e3070b6449c44d0b61ce8edb890b51f2
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-11-17 23:06:21 +0100

    repair

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,15 +1,11 @@
 const getApiUrl = (): string => {
-  if (import.meta.env.DEV) {
-    return "http://localhost:8000";
-  }
-
   const hostName = window.location.hostname;
 
   if (hostName === "localhost" || hostName === "127.0.0.1") {
     return "http://localhost:8000";
   }
 
-  return `https://${hostName}:8000`;
+  return `http://${hostName}:8000`;
 };
 
 export const API_URL = getApiUrl();
